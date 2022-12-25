a356bfa673cda84aded4ef499b3f722d8c64fab8
TA-800 <taherali0905@gmail.com>
2022-12-24 19:23:34 -0500

    added scorecheck and reload button to trivia game

--- frontend/src/components/Life.js
+++ frontend/src/components/Life.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import he from "he"; // html-escape
 
 export default function () {
     return (
@@ -17,6 +18,7 @@ function Questions() {
     const [questions, setQuestions] = useState([]);
     const [loading, setLoading] = useState(true);
     const [reload, setReload] = useState(false);
+    const parentRef = useRef(null);
 
     useEffect(() => {
         setLoading(true);
@@ -44,6 +46,52 @@ function Questions() {
             });
     }, [reload]);
 
+    function selectAnswer(event) {
+        const parent = event.target.parentNode;
+        const siblings = Array.from(parent.children).filter(
+            (child) => child !== event.target
+        );
+
+        siblings.forEach((sibling) => {
+            sibling.classList.remove("selected");
+        });
+
+        event.target.classList.add("selected");
+    }
+
+    function submitAnswer(event) {
+        let score = 0;
+        // The last question is the submit button which is not a question, so it will show up as undefined
+        const questionArray = Array.from(parentRef.current.children).forEach(
+            (question) => {
+                // Get the selected answer
+                const selectedAnswer = question.querySelector(".selected");
+                // Get the correct answer
+                const correctAnswer = question.dataset.answer
+                    ? he.unescape(question.dataset.answer)
+                    : undefined;
+                // Console log the result
+                console.log({
+                    selectedAnswer: selectedAnswer?.textContent,
+                    correctAnswer: correctAnswer,
+                });
+                // Check if the selected answer is correct
+                if (correctAnswer) {
+                    if (selectedAnswer?.textContent === correctAnswer) {
+                        score++;
+                    }
+                }
+            }
+        );
+        // console.log(`Score: ${score} / ${questions.length}`);
+
+        // Disable the button
+        event.target.disabled = true;
+        event.target.classList.add("disabled");
+        // Change the text to show the score
+        event.target.textContent = `Score: ${score} / ${questions.length}`;
+    }
+
     return (
         <>
             <div className="text-xl flex flex-row justify-between items-center mx-10">
@@ -62,47 +110,41 @@ function Questions() {
             {loading ? (
                 <div>Loading</div>
             ) : (
-                <ol className="trivia-wrapper">
+                <ol className="trivia-wrapper" ref={parentRef}>
                     {questions.map((q, i) => (
-                        <li key={i}>
+                        <li key={i} data-answer={q.correct_answer}>
                             <p className="trivia-question">
                                 {i + 1}.{" "}
-                                <strong>{unescapeHtml(q.question)}</strong>
+                                <strong>{he.unescape(q.question)}</strong>
                             </p>
                             <ul className="trivia-answer-wrapper">
                                 <li
                                     className="trivia-answer"
                                     onClick={(e) => selectAnswer(e)}>
-                                    <i>{unescapeHtml(q.correct_answer)}</i>
+                                    {he.unescape(q.correct_answer)}
                                 </li>
                                 {q.incorrect_answers.map((a, j) => (
                                     <li
                                         className="trivia-answer"
                                         onClick={(e) => selectAnswer(e)}
                                         key={j}>
-                                        {unescapeHtml(a)}
+                                        {he.unescape(a)}
                                     </li>
                                 ))}
                             </ul>
                         </li>
                     ))}
+                    <li className="flex justify-end text-lg m-10">
+                        <button
+                            className="btn btn-blue"
+                            onClick={(e) => submitAnswer(e)}>
+                            CHECK
+                        </button>
+                    </li>
                 </ol>
             )}
         </>
     );
-}
-
-function selectAnswer(event) {
-    const parent = event.target.parentNode;
-    const siblings = Array.from(parent.children).filter(
-        (child) => child !== event.target
-    );
-
-    siblings.forEach((sibling) => {
-        sibling.classList.remove("selected");
-    });
-
-    event.target.classList.add("selected");
 }
 
 // Reference: https://stackoverflow.com/questions/6234773/can-i-escape-html-special-chars-in-javascript
@@ -116,14 +158,3 @@ function selectAnswer(event) {
 // }
 
 // Opposite of the above function
-function unescapeHtml(safe) {
-    return safe
-        .replace(/&amp;/g, "&")
-        .replace(/&lt;/g, "<")
-        .replace(/&gt;/g, ">")
-        .replace(/&quot;/g, '"')
-        .replace(/&#039;/g, "'")
-        .replace(/&micro;/g, "µ")
-        .replace(/&Uuml/g, "Ü")
-        .replace(/&uuml;/g, "ü");
-}

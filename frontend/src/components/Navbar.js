f1617996ffc1e729ed03a6872ac3a56e38247e3f
TA-800 <taherali0905@gmail.com>
2022-12-25 23:50:39 -0500

    icons and tooltips

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,33 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { useMediaPredicate } from "react-media-hook";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faHome,
+    faUser,
+    faBook,
+    faLeaf,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
     const navigate = useNavigate();
     const location = useLocation();
+    const isSmallScreen = useMediaPredicate("(max-width: 640px)");
+    const navRef = useRef(null);
+
+    useEffect(() => {
+        // Add classes to nav buttons depending on the size/layout of the screen
+        // Replace the text of the buttons with icons by adding the "hide" class to the span element containing the text
+        if (isSmallScreen) {
+            navRef.current.childNodes.forEach((child) => {
+                child.childNodes[1].classList.add("hide");
+            });
+        } else {
+            navRef.current.childNodes.forEach((child) => {
+                child.childNodes[1].classList.remove("hide");
+            });
+        }
+    }, [isSmallScreen]);
 
     // Sidebar navigation
     return (
@@ -18,42 +43,50 @@ function Navbar() {
                 }}>
                 <img src={require("../menu.png")} alt="Menu icon" />
             </div>
-            <ul className="navigation">
+            <ul ref={navRef} className="navigation">
                 <li
+                    data-tooltip="Home"
                     onClick={() => navigate("/home")}
                     className={
                         location.pathname === "/home"
                             ? "active-navbar-button"
                             : ""
                     }>
-                    Home
+                    <FontAwesomeIcon icon={faHome} size="2x" />
+                    <span>Home</span>
                 </li>
                 <li
+                    data-tooltip="Study"
                     onClick={() => navigate("/study")}
                     className={
                         location.pathname === "/study"
                             ? "active-navbar-button"
                             : ""
                     }>
-                    Study
+                    <FontAwesomeIcon icon={faBook} size="2x" />
+                    <span>Study</span>
                 </li>
                 <li
+                    data-tooltip="Life"
                     onClick={() => navigate("/life")}
                     className={
                         location.pathname === "/life"
                             ? "active-navbar-button"
                             : ""
                     }>
-                    Life
+                    <FontAwesomeIcon icon={faLeaf} size="2x" />
+                    <span>Life</span>
                 </li>
                 <li
+                    data-tooltip="Account"
                     onClick={() => navigate("/account")}
                     className={
                         location.pathname === "/account"
                             ? "active-navbar-button"
                             : ""
                     }>
-                    Account
+                    <FontAwesomeIcon icon={faUser} size="2x" />
+                    <span>Account</span>
                 </li>
             </ul>
         </nav>
